cedb4dbbe0cb7909b5cd5c964d18d16a1f7800ac
anna-spb <vnukova.anna@gmail.com>
2022-06-25 17:04:54 +0300

    навела красоту в коде

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -9,13 +9,12 @@ public class MyFirstProgram {
         Point a1 = new Point(0, 3);
         Point a2 = new Point(2, 0);
 
-        a2.distance(a1);
-
         System.out.println(a2.distance(a1));
 
         // функция в main
         System.out.println(distance(a1, a2));
     }
+
     //функция в main
     public static double distance(Point p1, Point p2) {
         return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
