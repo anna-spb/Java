2f34c1c562e8ed1b06de4ace6587266b39690e67
anna-spb <vnukova.anna@gmail.com>
2022-06-25 20:04:03 +0300

    Созданы тесты для класса Point

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -1,12 +1,18 @@
 package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
 public class PointTests {
 
     @Test
-
-    public void testDistance(){
+    public void testDistance() {
         Point a1 = new Point(3, 0);
         Point a2 = new Point(0, 0);
-        assert a2.distance(a1) == 25;
+
+        //Assert.assertEquals(a2.distance(a1), 25);
+        //Assert.assertEquals(a2.distance(a1), 3);
+        Assert.assertEquals(a2.distance(a1), 3.0);
     }
 }
+
