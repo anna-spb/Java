83d96059a7e9a3d3e162745e790de354ce148c25
anna-spb <vnukova.anna@gmail.com>
2022-06-27 16:26:44 +0300

    созданы тесты

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -10,9 +10,33 @@ public class PointTests {
         Point a1 = new Point(3, 0);
         Point a2 = new Point(0, 0);
 
-        //Assert.assertEquals(a2.distance(a1), 25);
-        //Assert.assertEquals(a2.distance(a1), 3);
         Assert.assertEquals(a2.distance(a1), 3.0);
+
+    }
+
+    @Test
+    public void testDistanc1() {
+        Point a1 = new Point(3, 0);
+        Point a2 = new Point(0, 0);
+
+
+        Assert.assertEquals(a2.distance(a1), 3);
+    }
+
+    @Test
+    public void testDistance2() {
+        Point a1 = new Point(3, 0);
+        Point a2 = new Point(0, 0);
+
+        Assert.assertEquals(a2.distance(a1), 6.0);
+    }
+
+    @Test
+    public void testDistance3() {
+        Point a1 = new Point(0, 3);
+        Point a2 = new Point(2, 0);
+
+        Assert.assertEquals(a2.distance(a1), 3.60);
+
     }
 }
-
