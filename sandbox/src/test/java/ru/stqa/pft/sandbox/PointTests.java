49b3ff4c7fe7e5dd59b15d56b5977cbf2be023aa
anna-spb <vnukova.anna@gmail.com>
2022-06-29 14:17:56 +0300

    новые тесты метода TestDistance

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -16,27 +16,27 @@ public class PointTests {
 
     @Test
     public void testDistanc1() {
-        Point a1 = new Point(3, 0);
+        Point a1 = new Point(0, 0);
         Point a2 = new Point(0, 0);
 
 
-        Assert.assertEquals(a2.distance(a1), 3);
+        Assert.assertEquals(a2.distance(a1), 0.0);
     }
 
     @Test
     public void testDistance2() {
         Point a1 = new Point(3, 0);
-        Point a2 = new Point(0, 0);
+        Point a2 = new Point(-3, 0);
 
         Assert.assertEquals(a2.distance(a1), 6.0);
     }
 
     @Test
     public void testDistance3() {
-        Point a1 = new Point(0, 3);
-        Point a2 = new Point(2, 0);
+        Point a1 = new Point(0, 1);
+        Point a2 = new Point(0, -1);
 
-        Assert.assertEquals(a2.distance(a1), 3.60);
+        Assert.assertEquals(a2.distance(a1), 2.0);
 
     }
 }
