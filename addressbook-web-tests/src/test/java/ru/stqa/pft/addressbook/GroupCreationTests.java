4f83b12acce11fd4ede3ed82f75a9b366477a0df
anna-spb <vnukova.anna@gmail.com>
2022-07-02 20:10:09 +0300

    Выделена и оптимизирована фикстура

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -15,13 +15,7 @@ public class GroupCreationTests {
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
         wd = new ChromeDriver();
-
         wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
-    }
-
-    @Test
-    public void testGroupCreation() throws Exception {
         wd.get("http://localhost/addressbook/index.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -30,6 +24,12 @@ public class GroupCreationTests {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//input[@value='Login']")).click();
+
+    }
+
+    @Test
+    public void testGroupCreation() throws Exception {
+
         wd.findElement(By.linkText("groups")).click();
         wd.get("http://localhost/addressbook/group.php");
         wd.findElement(By.name("new")).click();
