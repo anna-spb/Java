c7517ee9366103c9050163dd14c465fe21446a98
anna-spb <vnukova.anna@gmail.com>
2022-07-16 17:27:21 +0300

    добавлено лямбда-выражение в создание групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,47 +4,29 @@ import java.util.Objects;
 
 public class GroupData {
     private final String name;
-    private final String id;
+
+
+    private int id;
     private final String header;
     private final String footer;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GroupData groupData = (GroupData) o;
-        return Objects.equals(name, groupData.name) && Objects.equals(id, groupData.id);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, id);
-    }
-
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "name='" + name + '\'' +
-                ", id='" + id + '\'' +
-                '}';
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    public GroupData (String name, String header, String footer) {
-        this.id = null;
+    public GroupData(String name, String header, String footer) {
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -55,8 +37,33 @@ public class GroupData {
         return header;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return id == groupData.id && Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+
     public String getFooter() {
         return footer;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
     }
 
 }
