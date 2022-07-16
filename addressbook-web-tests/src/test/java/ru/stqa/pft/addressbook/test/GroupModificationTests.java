67afc2084c343f6408d024cf37e5b40f32f1a0d9
anna-spb <vnukova.anna@gmail.com>
2022-07-16 15:38:31 +0300

    списки для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupModificationTests.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.UserData;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
 
     @Test
@@ -13,14 +15,14 @@ public class GroupModificationTests extends TestBase {
         if (!app.getGroupHelper().isThereAnyGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test5", null, null));
         }
-        int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().selectGroup();
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test3", "test2", "test3"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
     }
 
 }
