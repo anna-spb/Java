104ece66a3ddb87d9e9cad5fac9814d73a3d482b
anna-spb <vnukova.anna@gmail.com>
2022-08-06 19:30:44 +0300

    added testing in ModificationGRoup

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -21,6 +21,7 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private final String browser;
+    private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -31,6 +32,8 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        dbHelper = new DbHelper();
 
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
@@ -75,5 +78,9 @@ public class ApplicationManager {
 
     public UserHelper user() {
         return userHelper;
+    }
+
+    public DbHelper db() {
+        return dbHelper;
     }
 }
